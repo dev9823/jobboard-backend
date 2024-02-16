759ae07ef7d422a4ee621596a50886f1da86c371
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-02-16 21:55:08 +0300

    Add company model

--- accounts/models.py
+++ accounts/models.py
@@ -11,7 +11,24 @@ class JobSeeker(models.Model):
         primary_key=True,
     )
     slug = AutoSlugField(unique=True, populate_from="user")
-    image = models.ImageField(upload_to="jobseekers/images", blank=True, null=True)
+    image = models.ImageField(upload_to="image/job_seeker", blank=True, null=True)
     location = models.CharField(max_length=255)
     description = models.TextField()
     resume = models.FileField(upload_to="resume/")
+
+
+class Company(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        editable=False,
+    )
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(unique=True, populate_from="name")
+    phone = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/company")
+    location = models.CharField(max_length=255)
+    website = models.URLField(null=True, blank=True)
+    industry = models.CharField(max_length=255)
