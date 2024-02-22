daa02cd457d75fd75d64a03c4c83e3b3677cb7b4
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-02-23 00:43:04 +0300

    Add Applicant model

--- job/models.py
+++ job/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import Company
+from accounts.models import Company, JobSeeker
 
 
 class Vacancy(models.Model):
@@ -29,3 +29,18 @@ class Vacancy(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     job_type = models.CharField(max_length=1, choices=JOB_TYPE_CHOICES)
     deadline = models.DateField()
+
+
+class Applicant(models.Model):
+    applicant = models.ForeignKey(
+        JobSeeker, on_delete=models.CASCADE, related_name="applications"
+    )
+
+    vacancy = models.ForeignKey(
+        Vacancy, on_delete=models.CASCADE, related_name="applications"
+    )
+    cover_latter = models.TextField()
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("applicant", "vacancy")
